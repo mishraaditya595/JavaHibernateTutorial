7c0d67d7968bae2b1c643c6dd661d05c0d6ff1df
Aditya Mishra <mishraaditya595@gmail.com>
2025-04-29 16:09:44 +0530

    refactored code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,20 +11,23 @@ public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
 
-        s1.setSid(8);
-        s1.setName("Sagar");
-        s1.setMarks(27);
+        s1.setSid(9);
+        s1.setName("Sagarika");
+        s1.setMarks(31);
 
-        Configuration configuration = new Configuration();
-        configuration.addAnnotatedClass(Student.class);
-        configuration.configure();
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        SessionFactory sessionFactory = new Configuration()
+                .addAnnotatedClass(Student.class)
+                .configure()
+                .buildSessionFactory();
         Session session = sessionFactory.openSession();
 
         Transaction transaction = session.beginTransaction();
-        session.save(s1);
+        session.persist(s1);
         transaction.commit();
 
         System.out.println(s1);
+
+        session.close();
+        sessionFactory.close();
     }
 }
