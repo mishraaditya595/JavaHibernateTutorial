bc8b2d3466ecb889a1864d630f59b3962181ab8a
Aditya Mishra <mishraaditya595@gmail.com>
2025-04-29 11:18:06 +0530

    save data into db using hibernate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Student s1 = new Student();
 
+        s1.setSid(6);
+        s1.setName("Harsh");
+        s1.setMarks(14);
+
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(Student.class);
+        configuration.configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+        session.save(s1);
+        transaction.commit();
+
+        System.out.println(s1);
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private int sid;
+    private String name;
+    private int marks;
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "sid=" + sid +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
